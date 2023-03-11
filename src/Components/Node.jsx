b0b030182e90776baa0fef98eed8f8aa5e0b92fc
Zixi-Sam-Tang <zixi.sam.tang@gmail.com>
2023-03-10 22:56:53 -0500

    Updated styling for readability

--- src/Components/Node.jsx
+++ src/Components/Node.jsx
@@ -10,9 +10,28 @@ export function Node({row, col, onChange, val}) {
         setPrevVal(Number(e.target.value));
     }
 
+    const getClass = () => {
+        let temp = 'NodeInput ';
+        if(row % 3 == 0) {
+            temp = temp + 'BoldTop ';
+        }
+        if(row == 8) {
+            temp = temp + 'BoldBottom ';
+        }
+        if(col % 3 == 0) {
+            temp = temp + 'BoldLeft ';
+        }
+        if(col == 8) {
+            temp = temp + 'BoldRight ';
+        }
+
+        return temp;
+    }
+
     return (
         <div className="Node">
-            <input className="NodeInput" onChange={change}
+            <input className={getClass()} 
+                   onChange={change}
                    inputMode="numeric" type="text"
                    maxLength="1" autoComplete="off"
                    value={val === 0 ? "" : val.toString()}></input>
